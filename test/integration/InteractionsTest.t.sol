a3f915cbb1b4e8feb8f7c2eec343443567d58d35
Owolabi Adekunle Joseph <akoredeowolabi16@gmail.com>
2026-03-22 16:25:59 +0100

    Initial commit

---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interactions.s.sol";
+
+contract FundMeTestIntegration is Test {
+    FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
+    function setUp() external {
+        // No setup needed for integration tests
+        DeployFundMe deploy = new DeployFundMe();
+        fundMe = deploy.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        // Give the helper contract some ETH so it can call FundMe.fund{value: ...}()
+        vm.deal(address(fundFundMe), 0.01 ether);
+        fundFundMe.fundFundMe(address(fundMe));
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+
+        assert(address(fundMe).balance == 0);
+    }
+}
